be6b63accd92566393c2e7738e6e38b4c051fdd1
manuel5975p <flaschenholz@protonmail.com>
2021-04-13 21:26:23 +0200

    Init

--- flaschencat.cpp
+++ flaschencat.cpp
@@ -3,43 +3,23 @@
 #include <iostream>
 #include <vector>
 #include <ProgramOptions.hxx>
-#include <memmap.hpp>
-#include <time.hpp>
+#include <sys/types.h>
+#include <sys/stat.h>
 void do_file(int fd){
-    struct stat st;
-    fstat(fd, &st);
-    if(S_ISREG(st.st_mode) && false){
-        
-        constexpr size_t BS = 1 << 16;
-        auto m_size = lseek(fd, 0, SEEK_END);
-        //std::cerr << m_size << "\n";
-        char* data = (char*)mmap(0, m_size, (PROT_READ), MAP_SHARED, fd, 0);
-        write(STDOUT_FILENO, data, m_size);
-        /*for(size_t i = 0;i < m_size; i += BS){
-            size_t nbs = std::min(m_size - i, BS);
-            write(STDOUT_FILENO, data + i, nbs);
-        }*/
+    constexpr size_t N = 1 << 20;
+    char rdbuf[N];
+    ssize_t accum = 0;
+    loff_t offs = 0;
+    int mypipe[2];
+    if (pipe(mypipe)){
+        return;
     }
-    else{
-        constexpr size_t N = 1 << 20;
-        char rdbuf[N];
-        //auto timestmp = nanoTime();
-        ssize_t accum = 0;
-        loff_t offs = 0;
-        int mypipe[2];
-        if (pipe(mypipe)){
-            //std::cerr << "Pipe failed.\n";
-            return;
+    while(true){
+        auto n = splice(fd, nullptr, mypipe[1], nullptr, N, SPLICE_F_MORE);
+        if(n <= 0){
+            break;
         }
-        while(true){
-            auto n = splice(fd, nullptr, mypipe[1], nullptr, N, SPLICE_F_MORE);
-            //accum += n;
-            if(n <= 0){
-                break;
-            }
-
-            n = splice(mypipe[0], nullptr, STDOUT_FILENO, nullptr, N, SPLICE_F_MORE);
-        }
+        n = splice(mypipe[0], nullptr, STDOUT_FILENO, nullptr, N, SPLICE_F_MORE);
     }
 }
 int main(int argc, char** args){
